5335585384ad19835c3543a3730afb9c41a236cf
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2021-12-08 10:48:37 -0300

    Criação classe Gremlim.java

---
+++ src/main/java/Gremlim.java
@@ -0,0 +1,41 @@
+public class Gremlim implements Cloneable{
+
+    private float altura;
+    private String nome;
+    private boolean bagunceiro;
+
+    public Gremlim(float altura, String nome, boolean bagunceiro) {
+        this.altura = altura;
+        this.nome = nome;
+        this.bagunceiro = bagunceiro;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isBagunceiro() {
+        return bagunceiro;
+    }
+
+    public void setBagunceiro(boolean bagunceiro) {
+        this.bagunceiro = bagunceiro;
+    }
+
+    public Gremlim clone() throws CloneNotSupportedException {
+        Gremlim gremlimClone =(Gremlim)super.clone();
+        return gremlimClone;
+    }
+}
